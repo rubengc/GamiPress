2e2fdb8e2f86f33270fc348edb765261f67f04e8
Ruben Garcia <rubengcdev@gmail.com>
2019-12-16 11:16:28 +0100

    1.7.9
    **New Features**
    Improved GamiPress security by applying the WordPress security standards on the whole project (nonce checks, sanitization and escaping).
    Added no results message on Logs shortcode, block and widget.
    **Improvements**
    Awards engine performance improvement by better filter specific triggered events.
    Improved placement of server messages given by the unlock using points process.
    Nonce usage on all ajax functions to improve security and veracity of ajax requests.
    Added sanitization on all requests fields.
    Added escaping on all HTML templates where needed.
    Added disabled effect styles to Select2.
    Code reduction on several functions.
    CSS improvements on the admin area.
    Removed support to the Dark Mode plugin since WordPress core is working on a new one.
    **Developer Notes**
    Several improvements on gamipress_get_triggered_requirements() function.

--- gamipress.php
+++ gamipress.php
@@ -3,7 +3,7 @@
  * Plugin Name:     	GamiPress
  * Plugin URI:      	https://gamipress.com
  * Description:     	The most flexible and powerful gamification system for WordPress.
- * Version:         	1.7.9-beta
+ * Version:         	1.7.9
  * Author:          	GamiPress
  * Author URI:      	https://gamipress.com/
  * Text Domain:     	gamipress
@@ -136,7 +136,7 @@ final class GamiPress {
 	private function constants() {
 
 		// Plugin version
-		define( 'GAMIPRESS_VER', '1.7.9-beta' );
+		define( 'GAMIPRESS_VER', '1.7.9' );
 
 		// Plugin file
 		define( 'GAMIPRESS_FILE', __FILE__ );
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: gamipress, tsunoa, rubengc, eneribs
 Tags: gamipress, gamification, points, achievements, ranks, badges, awards, rewards, credits, engagement, leaderboard, emails, notifications, progress
 Requires at least: 4.4
 Tested up to: 5.3
-Stable tag: 1.7.8
+Stable tag: 1.7.9
 License: GNU AGPL v3.0
 License URI: http://www.gnu.org/licenses/agpl-3.0.html
 
